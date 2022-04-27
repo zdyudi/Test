1d007fc87e15ff7dd9edadcd8438cc935fab8c62
zdyudi <1175813308@qq.com>
2022-04-27 15:44:00 +0800

    <vs>

--- test.4.26/test.c
+++ test.4.26/test.c
@@ -95,21 +95,109 @@
 //	return 0;
 //}
 
-int main()
-{
-	int arr1[] = { 1,2,3,4,5 };
-	int arr2[] = { 2,3,4,5,6 };
-	int arr3[] = { 3,4,5,6,7 };
-	int* parr[] = {arr1,arr2,arr3};
-	int i = 0;
-	for (i = 0; i < 3; i++)
-	{
-		int j = 0;
-		for (j = 0; j < 5; j++)
-		{
-			printf("%d ", *(parr[i] + j));
-		}
-		printf("\n");
-	}
-	return 0;
-}
+//int main()
+//{
+//	int arr1[] = { 1,2,3,4,5 };
+//	int arr2[] = { 2,3,4,5,6 };
+//	int arr3[] = { 3,4,5,6,7 };
+//	int* parr[] = {arr1,arr2,arr3};
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < 5; j++)
+//		{
+//			printf("%d ", *(parr[i] + j));
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//数组指针 - 指针
+//int main()
+//{
+//	//int* p = NULL;//整形指针 - 指向整形的指针 - 可以存放整形的地址
+//	//char* pc = NULL;//字符指针 - 指向字符的的指针 - 可以存放字符的地址
+//	          //数组指针 - 指向数组的指针 - 存放数组的地址
+//	//int arr[10] = { 0 };
+//	//arr - 首元素地址
+//	//&arr[0] - 首元素地址
+//	//&arr - 数组的地址
+//
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int(*p)[10] = &arr;//数组的地址要存起来
+//	//p就是数组指针
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	char* arr[5];
+//	char* (*pa)[5] = &arr;
+//
+//	int arr2[10] = { 0 };
+//	int(*pa2)[10] = &arr2;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int* p = arr;
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", *(p + i));
+//	}
+//	/*int(*pa)[10] = &arr;
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", (*pa)[i]);
+//	}*/
+//	return 0;
+//}
+
+//参数是数组的形式
+//void print1(int arr[3][5], int x, int y)
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < x; i++)
+//	{
+//		for (j = 0; j < y; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//}
+
+//参数是指针的形式
+//void print2(int(*p)[5], int x, int y)
+//{
+//	int i = 0;
+//	for (i = 0; i < x; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < y; j++)
+//		{
+//			//printf("%d ", *(*(p + i) + j));
+//			printf("%d ", ( *(p + i))[j]);
+//			//printf("%d ", p[i][j]);
+//			//printf("%d ", *(p[i] + j));
+//		}
+//		printf("\n");
+//	}
+//}
+
+//int main()
+//{
+//	int arr[3][5] = { {1,2,3,4,5},{2,3,4,5,6},{3,4,5,6,7} };
+//	print1(arr,3,5);
+//	print2(arr, 3, 5);
+//	return 0;
+//}
