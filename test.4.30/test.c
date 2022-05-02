ab6e72387653c4e0053fd8c2976ac0c68f503808
zdyudi <1175813308@qq.com>
2022-05-02 15:46:08 +0800

    <vs>

--- test.4.30/test.c
+++ test.4.30/test.c
@@ -123,14 +123,103 @@
 //}
 
 
-void BubbleSort(int arr[], int sz)
+//void BubbleSort(int arr[], int sz)
+//{
+//	int i = 0;
+//	//趟数
+//	for (i = 0; i < sz - 1; i++)
+//	{
+//		//一趟冒泡排序
+//		int j = 0;
+//		for (j = 0; j < sz - 1 - i; j++)
+//		{
+//			if (arr[j] > arr[j + 1])
+//			{
+//				int tmp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = tmp;
+//			}
+//		}
+//	}
+//}
 
+//struct Stu
+//{
+//	char name[20];
+//	int age;
+//};
+
+#include<stdlib.h>
+//int cmp_int(const void* e1, const void* e2)
+//{
+//	//比较两个整形值的
+//	return *(int*)e1 - *(int*)e2;
+//}
+
+//void test1()
+//{
+//	int arr[] = { 1,3,5,7,9,2,4,6,8,0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	qsort(arr, sz, sizeof(arr[0]), cmp_int);
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//}
+
+//int cmp_float(const void* e1, const void* e2)
+//{
+//	return *(float*)e1 - *(float*)e2;
+//}
+
+//void test2()
+//{
+//	float f[] = { 9.0,8.0,7.0,6.0,5.0 };
+//	int sz = sizeof(f) / sizeof(f[0]);
+//	qsort(f, sz, sizeof(f[0]), cmp_float);
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%f ", f[i]);
+//	}
+//}
+
+#include<string.h>
+//int com_stu_by_age(const void* e1, const void* e2)
+//{
+//	return ((struct Stu*)e1)->age - ((struct Stu*)e2)->age;
+//}
+
+//int com_stu_by_name(const void* e1, const void* e2)
+//{
+//	//比较名字就是比较字符串
+//	//字符串比较不能直接用><=来比较,应该用strcmp函数
+//	return strcmp(((struct Stu*)e1)->name,((struct Stu*)e2)->name);
+//}
+//void test3()
+//{
+//	struct Stu s[3] = { {"张三",20},{"李四",25},{"王五",30} };
+//	int sz = sizeof(s) / sizeof(s[0]);
+//	qsort(s, sz, sizeof(s[0]), com_stu_by_age);
+//}
 //qsort - 可以排序任意类型的数据
+
+void test4()
+{
+
+}
 int main()
 {
 	//冒泡排序函数
-	int arr[] = { 1,3,5,7,9,2,4,6,8,0 };
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	BubbleSort(arr, sz);
+	
+	
+	//test1();
+	//test2();
+	//test3();
+	test4();
+	//BubbleSort(arr, sz);
+	
 	return 0;
 }
+
