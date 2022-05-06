9352ac351f13cd205a8999f5770132ec63f1d8bf
zdyudi <1175813308@qq.com>
2022-05-06 15:35:55 +0800

    <vs>

--- test.5.4/test.c
+++ test.5.4/test.c
@@ -1,4 +1,4 @@
-
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
 //练习
@@ -90,15 +90,132 @@
 //	return 0;
 //}
 
+//int main()
+//{
+//	char* c[] = { "ENTER","NEW","POINT","FIRST" };
+//	char** cp[] = { c + 3,c + 2,c + 1,c };
+//	char*** cpp = cp;
+//
+//	printf("%s\n", **++cpp);//"POINT"
+//	printf("%s\n", *-- * ++cpp + 3);// ER
+//	printf("%s\n", *cpp[-2] + 3);// ST
+//	printf("%s\n", cpp[-1][-1] + 1);// EW
+//	return 0;
+//}
+
+#include<string.h>
+#include<assert.h>
+//void reverse(char* str)
+//{
+//	assert(str);
+//	int len = strlen(str);
+//	char* left = str;
+//	char* right = str + len - 1;
+//	while (left<right)
+//	{
+//		char tmp = *left;
+//		*left = *right;
+//		*right = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+
+//int main()
+//{
+//	char arr[256] = { 0 };
+//	//scanf("%s", arr);
+//	gets(arr);//读取一行
+//	//逆序函数
+//	reverse(arr);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 0;
+//	int n = 0;
+//	scanf("%d%d", &a, &n);
+//	int sum = 0;
+//	int i = 0;
+//	int ret = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		ret = ret * 10 + a;
+//		sum += ret;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+#include<math.h>
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i < 100000; i++)
+//	{
+//		//判断i是否为水仙花数（自幂数）
+//		//1.计算i的位数 n位数
+//		int n = 1;
+//		int tmp = i;
+//		int sum = 0;
+//		while (tmp /= 10)
+//		{
+//			n++;
+//		}
+//		//2.计算i的每一位的n次方之和 sum
+//		tmp = i;
+//		while (tmp)
+//		{
+//			sum += pow(tmp % 10, n);
+//			tmp /= 10;
+//		}
+//		//3.比较i == sum
+//		if (i == sum)
+//		{
+//			printf("%d ", i);
+//		}
+//	}
+//	return 0;
+//}
+
 int main()
 {
-	char* c[] = { "ENTER","NEW","POINT","FIRST" };
-	char** cp[] = { c + 3,c + 2,c + 1,c };
-	char*** cpp = cp;
-
-	printf("%s\n", **++cpp);//"POINT"
-	printf("%s\n", *-- * ++cpp + 3);// ER
-	printf("%s\n", *cpp[-2] + 3);// ST
-	printf("%s\n", cpp[-1][-1] + 1);// EW
+	int line = 0;
+	scanf("%d", &line);
+	//打印上半部分
+	int i = 0;
+	for (i = 0; i < line; i++)
+	{
+		//打印空格
+		int j = 0;
+		for (j = 0; j < line-1-i; j++)
+		{
+			printf(" ");
+		}
+	    //打印*
+		for (j = 0; j < 2*i+1; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	//打印下半部分
+	for (i = 0; i < line - 1; i++)
+	{
+		//打印空格
+		int j = 0;
+		for (j = 0; j <= i; j++)
+		{
+			printf(" ");
+		}
+		//打印*
+		for (j = 0; j < 2 * (line-1-i) - 1; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
 	return 0;
 }
