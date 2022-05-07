071abd2d8d8c0c8ebaa2ddf3fa438d8662b1d368
zdyudi <1175813308@qq.com>
2022-05-07 16:04:06 +0800

    <vs>

--- test.5.4/test.c
+++ test.5.4/test.c
@@ -180,42 +180,111 @@
 //	return 0;
 //}
 
+//int main()
+//{
+//	int line = 0;
+//	scanf("%d", &line);
+//	//打印上半部分
+//	int i = 0;
+//	for (i = 0; i < line; i++)
+//	{
+//		//打印空格
+//		int j = 0;
+//		for (j = 0; j < line-1-i; j++)
+//		{
+//			printf(" ");
+//		}
+//	    //打印*
+//		for (j = 0; j < 2*i+1; j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	//打印下半部分
+//	for (i = 0; i < line - 1; i++)
+//	{
+//		//打印空格
+//		int j = 0;
+//		for (j = 0; j <= i; j++)
+//		{
+//			printf(" ");
+//		}
+//		//打印*
+//		for (j = 0; j < 2 * (line-1-i) - 1; j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int money = 0;
+//	int total = 0;
+//	int empty = 0;
+//	scanf("%d", &money);
+//	//买回来的汽水喝掉
+//	//if (money == 0)
+//	//	total = 0;
+//	//else
+//	//	total = 2 * money - 1;
+//
+//	total = money;
+//	empty = money;
+//	//换回来的汽水
+//	while (empty >= 2)
+//	{
+//		total += empty / 2;
+//		empty = empty / 2 + empty % 2;
+//	}
+//	printf("total=%d\n", total);
+//	return 0;
+//}
+
+void move(int arr[], int sz)
+{
+	int left = 0;
+	int right = sz - 1;
+	int tmp = 0;
+	while (left<right)
+	{
+		//从左边找偶数
+		while ((left < right) && arr[left] % 2 == 1)
+		{
+			left++;
+		}
+		//从右边找奇数
+		while ((left < right) && arr[right] % 2 == 0)
+		{
+			right--;
+		}
+		if (left < right)
+		{
+			int tmp = arr[left];
+			arr[left] = arr[right];
+			arr[right] = tmp;
+		}
+	}
+}
+
+void print(int arr[], int sz)
+{
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d\n", arr[i]);
+	}
+	printf("\n");
+}
+
 int main()
 {
-	int line = 0;
-	scanf("%d", &line);
-	//打印上半部分
-	int i = 0;
-	for (i = 0; i < line; i++)
-	{
-		//打印空格
-		int j = 0;
-		for (j = 0; j < line-1-i; j++)
-		{
-			printf(" ");
-		}
-	    //打印*
-		for (j = 0; j < 2*i+1; j++)
-		{
-			printf("*");
-		}
-		printf("\n");
-	}
-	//打印下半部分
-	for (i = 0; i < line - 1; i++)
-	{
-		//打印空格
-		int j = 0;
-		for (j = 0; j <= i; j++)
-		{
-			printf(" ");
-		}
-		//打印*
-		for (j = 0; j < 2 * (line-1-i) - 1; j++)
-		{
-			printf("*");
-		}
-		printf("\n");
-	}
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	move(arr,sz);
+	print(arr, sz);
 	return 0;
 }
