359264f608b9d121b0404f916c75012cf7623be0
zdyudi <1175813308@qq.com>
2022-05-08 16:09:32 +0800

    <vs>

--- test.5.4/test.c
+++ test.5.4/test.c
@@ -244,47 +244,161 @@
 //	return 0;
 //}
 
-void move(int arr[], int sz)
+//void move(int arr[], int sz)
+//{
+//	int left = 0;
+//	int right = sz - 1;
+//	int tmp = 0;
+//	while (left<right)
+//	{
+//		//´Ó×ó±ßÕÒÅ¼Êý
+//		while ((left < right) && arr[left] % 2 == 1)
+//		{
+//			left++;
+//		}
+//		//´ÓÓÒ±ßÕÒÆæÊý
+//		while ((left < right) && arr[right] % 2 == 0)
+//		{
+//			right--;
+//		}
+//		if (left < right)
+//		{
+//			int tmp = arr[left];
+//			arr[left] = arr[right];
+//			arr[right] = tmp;
+//		}
+//	}
+//}
+//
+//void print(int arr[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d\n", arr[i]);
+//	}
+//	printf("\n");
+//}
+//
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	move(arr,sz);
+//	print(arr, sz);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	int d = 0;
+//	int e = 0;
+//	for (a = 1; a <= 5; a++)
+//	{
+//		for (b = 1; b <= 5; b++)
+//		{
+//			for (c = 1; c <= 5; c++)
+//			{
+//				for (d = 1; d <= 5; d++)
+//				{
+//					for (e = 1; e <= 5; e++)
+//					{
+//						if (((b==2) + (a==3) == 1) &&
+//							((b==2) + (a==4) == 1) &&
+//							((c==1) + (d==2) == 1) &&
+//							((c==5) + (d==3) == 1) &&
+//							((e==4) + (a==1) == 1))
+//						{
+//							if(a*b*c*d*e == 120)
+//							   printf("a=%d b=%d c=%d d=%d e=%d\n", a, b, c, d,e);
+//						}
+//					}
+//				}
+//			}
+//		}
+//	}
+//	return 0;
+//}
+
+//±©Á¦Çó½â·¨
+//void left_move(char* arr, int k)
+//{
+//	assert(arr != NULL);
+//	int i = 0;
+//	int len = strlen(arr);
+//	for (i = 0; i < k; i++)
+//	{
+//		//1.
+//		char tmp = *arr;
+//		//2.
+//		int j = 0;
+//		for (j = 0; j < len-1; j++)
+//		{
+//			*(arr + j) = *(arr + j + 1);
+//		}
+//		//3.
+//		*(arr + len - 1) = tmp;
+//	}
+//}
+
+//Èý²½·­×ª·¨
+void reverse(char* left, char* right)
 {
-	int left = 0;
-	int right = sz - 1;
-	int tmp = 0;
+	assert(left != NULL);
+	assert(right != NULL);
 	while (left<right)
 	{
-		//´Ó×ó±ßÕÒÅ¼Êý
-		while ((left < right) && arr[left] % 2 == 1)
-		{
-			left++;
-		}
-		//´ÓÓÒ±ßÕÒÆæÊý
-		while ((left < right) && arr[right] % 2 == 0)
-		{
-			right--;
-		}
-		if (left < right)
-		{
-			int tmp = arr[left];
-			arr[left] = arr[right];
-			arr[right] = tmp;
-		}
+		char tmp = *left;
+		*left = *right;
+		*right = tmp;
+		left++;
+		right--;
 	}
 }
 
-void print(int arr[], int sz)
+void left_move(char* arr, int k)
 {
+	assert(arr);
+	int len = strlen(arr);
+	assert(k <= len);
+	reverse(arr, arr + k - 1);//ÄæÐò×ó±ß
+	reverse(arr + k, arr + len - 1);//ÄæÐòÓÒ±ß
+	reverse(arr, arr + len - 1);//ÄæÐòÕûÌå
+}
+
+int is_left_move(char* s1, char* s2)
+{
+	int len = strlen(s1);
 	int i = 0;
-	for (i = 0; i < sz; i++)
+	for (i = 0; i < len; i++)
 	{
-		printf("%d\n", arr[i]);
+		left_move(s1, 1);
+		int ret = strcmp(s1, s2);
+		if (ret == 0)
+			return 1;
 	}
-	printf("\n");
+	return 0;
 }
 
 int main()
 {
-	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	move(arr,sz);
-	print(arr, sz);
+	char arr1[] = "abcdef";
+	char arr2[] = "cdefab";
+	int ret=is_left_move(arr1, arr2);
+	if (ret == 1)
+		printf("Yes\n");
+	else
+		printf("No\n");
 	return 0;
 }
+
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	left_move(arr, 4);
+//	printf("%s\n", arr);
+//	return 0;
+//}
