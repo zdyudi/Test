efed93971e735c0c2cf206557bd8fa55c0b7538a
zdyudi <1175813308@qq.com>
2022-05-16 15:56:03 +0800

    <vs>

--- test.5.14/test.c
+++ test.5.14/test.c
@@ -113,3 +113,99 @@
 	printf("after: a=%d b=%d\n", a, b);
 	return 0;
 }*/
+
+
+//声明一个结构体类型
+//声明一个学生类型，是想通过学生类型来创建学生变量（对象）
+//描述学生：属性-名字+电话+性别+年龄
+
+//struct Stu
+//{
+//	char name[20];
+//	char tele[12];
+//	char sex[10];
+//	int age;
+//}s4,s5,s6;//全局变量
+//
+//struct Stu s3;//全局变量
+//
+//int main()
+//{
+//	//创建的结构体变量
+//	struct Stu s1;
+//	struct Stu s2;
+//
+//	return 0;
+//}
+
+//struct
+//{
+//	int a;
+//	char c;
+//}sa;
+//
+//struct
+//{
+//	int a;
+//	char c;
+//}* psa;
+//
+//int main()
+//{
+//	psa = &sa;//两者不相同,是两个类型
+//	return 0;
+//}
+
+//结构体的自引用
+//typedef struct Node//重命名
+//{
+//	int date;//4
+//	struct Node* next;//4/8
+//}Node;
+
+//struct T
+//{
+//	double weight;
+//	short age;
+//};
+//
+//struct s
+//{
+//	char c;
+//	struct T st;
+//	int a;
+//	double d;
+//	char arr[20];
+//};
+//
+//int main()
+//{
+//	//struct s s = { 'c',100,3.14,"hello bit" };
+//	struct s s = { 'c',{55.6,30},100,3.14,"hello bit" };
+//	printf("%c %d %lf %s\n", s.c, s.a, s.d, s.arr);
+//	printf("%lf\n", s.st.weight);
+//	return 0;
+//}
+
+struct S1
+{
+	char c1;
+	int a;
+	char c2;
+};
+
+struct S2
+{
+	char c1;
+	char c2;
+	int a;
+};
+
+int main()
+{
+	struct S1 s1 = { 0 };
+	printf("%d\n",sizeof(s1) );//
+	struct S2 s2 = { 0 };
+	printf("%d\n",sizeof(s2) );//
+	return 0;
+}
