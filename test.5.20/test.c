fdd5f99338249d65608835e7127ee8697be25587
zdyudi <1175813308@qq.com>
2022-05-21 14:00:38 +0800

    <vs>

--- test.5.20/test.c
+++ test.5.20/test.c
@@ -27,14 +27,79 @@
 //}
 
 //联合 - 联合体 - 共用体
+//union Un
+//{
+//	char c;
+//	int i;
+//};
+//int main()
+//{
+//	union Un u;
+//	printf("%d\n", sizeof(u));//4
+//	printf("%p\n", &u);
+//	printf("%p\n", &(u.c));
+//	printf("%p\n", &(u.i));
+//	return 0;
+//}
+
+//int check_sys()
+//{
+//	/*int a = 1;
+//	return *(char*)&a;*/
+//	union Un
+//	{
+//		char c;
+//		int i;
+//	}u;
+//	u.i = 1;
+//	return u.c;
+//}
+
+//int main()
+//{
+//	int a = 1;
+//	int ret = check_sys();
+//	if (1 == ret)
+//	{
+//		printf("小端\n");
+//	}
+//	else
+//	{
+//		printf("大端\n");
+//	}
+//
+//	//int a = 0x11223344;
+//	//低地址 --------------》高地址
+//	//     11 22 33 44                   大端字节序存储模式
+//	//     44 33 22 11                   小段字节序存储模式
+//	//讨论一个数据，放在内存中的存放的字节顺序
+//	//大小端字节序问题
+//	return 0;
+//}
+
+//enum Sex
+//{
+//	MALE,
+//	FEMALE,
+//	SECRET
+//};
+//
+//int main()
+//{
+//	enum Sex s = MALE;
+//	printf("%d\n", sizeof(s));
+//	return 0;
+//}
+
 union Un
 {
-	char c;
-	int i;
+	int a;//4
+	char arr[5];//5
 };
+
 int main()
 {
 	union Un u;
-	printf("%d\n", sizeof(u));//4
+	printf("%d\n", sizeof(u));//8
 	return 0;
 }
