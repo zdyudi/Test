dd867a5a0ff1e5b4e5ba52594708ead618322a43
zdyudi <1175813308@qq.com>
2022-05-27 14:05:13 +0800

    <vs>

--- test.5.20/test.c
+++ test.5.20/test.c
@@ -1,4 +1,4 @@
-
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
 //枚举类型
@@ -91,15 +91,62 @@
 //	return 0;
 //}
 
-union Un
-{
-	int a;//4
-	char arr[5];//5
-};
+//union Un
+//{
+//	int a;//4
+//	char arr[5];//5
+//};
+//
+//int main()
+//{
+//	union Un u;
+//	printf("%d\n", sizeof(u));//8
+//	return 0;
+//}
+
+//struct S
+//{
+//	char name[20];
+//	int age
+//};
+//
+//int main()
+//{
+//	struct S arr[50];//50个struct S类型的数据
+//	//30 - 浪费
+//	//60 - 不够
+//	return 0;
+//}
+
+#include<stdlib.h>
+#include<string.h>
+#include<errno.h>
 
 int main()
 {
-	union Un u;
-	printf("%d\n", sizeof(u));//8
+	//向内存申请10个整形的空间
+	int* p = (int*)malloc(10 * sizeof(int));
+	if (p == NULL)
+	{
+		//打印错误原因的一个方式
+		printf("%s\n", strerror(errno));
+	}
+	else
+	{
+		//正常使用空间
+		int i = 0;
+		for (i = 0; i < 10; i++)
+		{
+			*(p + i) = i;
+		}
+		for (i = 0; i < 10; i++)
+		{
+			printf("%d ", *(p + i));
+		}
+	}
+	//当动态申请的空间不再使用
+	//就应该还给操作系统
+	free(p);
+	p = NULL;
 	return 0;
 }
