f383b9ab5999763ec409a69ddbd2a90b2a345ccb
zdyudi <1175813308@qq.com>
2022-05-31 14:02:00 +0800

    <vs>

--- test_5.30/test.c
+++ test_5.30/test.c
@@ -43,21 +43,57 @@
 //	return 0;
 //}
 
-void GetMemory(char**p,int num)
-{
-	*p = (char*)malloc(num);
-}
+//void GetMemory(char**p,int num)
+//{
+//	*p = (char*)malloc(num);
+//}
+//
+//void Test()
+//{
+//	char* str = NULL;
+//	GetMemory(&str,100);
+//	strcpy(str, "hello");
+//	printf(str);
+//
+//	free(str);
+//	str=NULL:
+//}
+//
+//int main()
+//{
+//	Test();
+//	return 0;
+//}
 
-void Test()
+struct S
 {
-	char* str = NULL;
-	GetMemory(&str,100);
-	strcpy(str, "hello");
-	printf(str);
-}
+	int n;
+	int arr[0];//ÈáÐÔÊý×é
+};
 
 int main()
 {
-	Test();
+	struct S* ps = (struct S*)malloc(sizeof(struct S)+5*sizeof(int));
+	ps->n = 100;
+	int i = 0;
+	for (i = 0; i < 5; i++)
+	{
+		ps->arr[i] = i;
+	}
+	struct S* ptr = realloc(ps, 44);
+	if (ptr != NULL)
+	{
+		ps = ptr;
+	}
+	for (i = 5; i < 10; i++)
+	{
+		ps->arr[i] = i;
+	}
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d ", ps->arr[i]);
+	}
+	free(ps);
+	ps = NULL;
 	return 0;
 }
