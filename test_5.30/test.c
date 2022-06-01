d5deb505d1d6ccbfa4e2e91916f672babd8c4afa
zdyudi <1175813308@qq.com>
2022-06-01 13:54:44 +0800

    <vs>

--- test_5.30/test.c
+++ test_5.30/test.c
@@ -65,25 +65,63 @@
 //	return 0;
 //}
 
+//struct S
+//{
+//	int n;
+//	int arr[0];//柔性数组
+//};
+//
+//int main()
+//{
+//	struct S* ps = (struct S*)malloc(sizeof(struct S)+5*sizeof(int));
+//	ps->n = 100;
+//	int i = 0;
+//	for (i = 0; i < 5; i++)
+//	{
+//		ps->arr[i] = i;
+//	}
+//	struct S* ptr = realloc(ps, 44);
+//	if (ptr != NULL)
+//	{
+//		ps = ptr;
+//	}
+//	for (i = 5; i < 10; i++)
+//	{
+//		ps->arr[i] = i;
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", ps->arr[i]);
+//	}
+//	free(ps);
+//	ps = NULL;
+//	return 0;
+//}
+
 struct S
 {
 	int n;
-	int arr[0];//柔性数组
+	int* arr;
 };
 
 int main()
 {
-	struct S* ps = (struct S*)malloc(sizeof(struct S)+5*sizeof(int));
-	ps->n = 100;
+	struct S* ps = (struct S*)malloc(sizeof(struct S));
+	ps->arr = malloc(5 * sizeof(int));
 	int i = 0;
 	for (i = 0; i < 5; i++)
 	{
 		ps->arr[i] = i;
 	}
-	struct S* ptr = realloc(ps, 44);
+	for (i = 0; i < 5; i++)
+	{
+		printf("%d ", ps->arr[i]);
+	}
+	//调整大小
+	int* ptr = realloc(ps->arr, 10 * sizeof(int));
 	if (ptr != NULL)
 	{
-		ps = ptr;
+		ps->arr = ptr;
 	}
 	for (i = 5; i < 10; i++)
 	{
@@ -93,6 +131,8 @@ int main()
 	{
 		printf("%d ", ps->arr[i]);
 	}
+	free(ps->arr);
+	ps->arr = NULL;
 	free(ps);
 	ps = NULL;
 	return 0;
