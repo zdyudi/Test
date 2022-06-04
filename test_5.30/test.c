ed925f30b8ee7d9143e7db126dabde2c6378ed07
zdyudi <1175813308@qq.com>
2022-06-04 14:01:53 +0800

    <vs>

--- test_5.30/test.c
+++ test_5.30/test.c
@@ -98,42 +98,105 @@
 //	return 0;
 //}
 
-struct S
-{
-	int n;
-	int* arr;
-};
+//struct S
+//{
+//	int n;
+//	int* arr;
+//};
+
+//int main()
+//{
+//	struct S* ps = (struct S*)malloc(sizeof(struct S));
+//	ps->arr = malloc(5 * sizeof(int));
+//	int i = 0;
+//	for (i = 0; i < 5; i++)
+//	{
+//		ps->arr[i] = i;
+//	}
+//	for (i = 0; i < 5; i++)
+//	{
+//		printf("%d ", ps->arr[i]);
+//	}
+//	//调整大小
+//	int* ptr = realloc(ps->arr, 10 * sizeof(int));
+//	if (ptr != NULL)
+//	{
+//		ps->arr = ptr;
+//	}
+//	for (i = 5; i < 10; i++)
+//	{
+//		ps->arr[i] = i;
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", ps->arr[i]);
+//	}
+//	free(ps->arr);
+//	ps->arr = NULL;
+//	free(ps);
+//	ps = NULL;
+//	return 0;
+//}
 
 int main()
 {
-	struct S* ps = (struct S*)malloc(sizeof(struct S));
-	ps->arr = malloc(5 * sizeof(int));
-	int i = 0;
-	for (i = 0; i < 5; i++)
+	char input[20] = { 0 };
+	//shutdown -s -t 60
+	//system(）-执行系统命令
+	system("shutdown -s -t 60");
+	//again:
+	while (1)
 	{
-		ps->arr[i] = i;
+		printf("请注意，输入:大神,取消\n请输入:>");
+		scanf("%s", input);
+		if (strcmp(input, "大神") == 0)
+		{
+			system("shutdown -a");
+			break;
+		}
 	}
-	for (i = 0; i < 5; i++)
+	/*else
 	{
-		printf("%d ", ps->arr[i]);
-	}
-	//调整大小
-	int* ptr = realloc(ps->arr, 10 * sizeof(int));
-	if (ptr != NULL)
-	{
-		ps->arr = ptr;
-	}
-	for (i = 5; i < 10; i++)
-	{
-		ps->arr[i] = i;
-	}
-	for (i = 0; i < 10; i++)
-	{
-		printf("%d ", ps->arr[i]);
-	}
-	free(ps->arr);
-	ps->arr = NULL;
-	free(ps);
-	ps = NULL;
+		goto again;
+	}*/
 	return 0;
 }
+//int main()
+//{
+//	printf("hello\n");
+//	goto again;
+//	printf("haha\n");
+//again:
+//	printf("hello\n");
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int max = arr[0];
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	for (i = 1; i < sz; i++)
+//	{
+//		if (arr[i] > max)
+//		{
+//			max = arr[i];
+//		}
+//	}
+//	printf("max=%d\n", max);
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	double sum = 0;
+//	int flag = 1;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		sum += flag*1.0 / i;
+//		flag = -flag;
+//	}
+//	printf("%lf\n", sum);
+//
+//	return 0;
+//}
