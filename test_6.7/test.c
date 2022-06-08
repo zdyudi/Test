6eb7808031753ffb60b765506873177919f1b80d
zdyudi <1175813308@qq.com>
2022-06-08 13:50:26 +0800

    <vs>

--- test_6.7/test.c
+++ test_6.7/test.c
@@ -64,22 +64,69 @@
 //	return 0;
 //}
 
+//struct S
+//{
+//	int n;
+//	float score;
+//	char arr[10];
+//};
+//
+//int main()
+//{
+//	struct S s = { 100,3.14,"abcdef" };
+//	struct S tmp = { 0 };
+//	char buf[1024] = { 0 };
+//
+//	sprintf(buf, "%d %f %s", s.n, s.score, s.arr);
+//	//printf("%s\n", buf);
+//	sscanf(buf, "%d %f %s", &(tmp.n), &(tmp.score), tmp.arr);
+//	printf("%d %f %s", tmp.n, tmp.score, tmp.arr);
+//	return 0;
+//}
+
+//struct S
+//{
+//	char name[20];
+//	int age;
+//	double score;
+//};
+//
+//int main()
+//{
+//	//struct S s = { "ÕÅÈý",20,55.6 };
+//	struct S tmp = { 0 };
+//	FILE* pf = fopen("test.txt", "rb");
+//	if (pf == NULL)
+//	{
+//		return 0;
+//	}
+//	//write
+//	fread(&tmp, sizeof(struct S), 1, pf);
+//	printf("%s %d %lf\n", tmp.name, tmp.age, tmp.score);
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[5] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < 5; i++)
+//	{
+//		printf("%d\n",arr[i]);
+//	}
+//	return 0;
+//}
+
 struct S
 {
-	int n;
-	float score;
-	char arr[10];
+	int age;
 };
 
 int main()
 {
-	struct S s = { 100,3.14,"abcdef" };
-	struct S tmp = { 0 };
-	char buf[1024] = { 0 };
-
-	sprintf(buf, "%d %f %s", s.n, s.score, s.arr);
-	//printf("%s\n", buf);
-	sscanf(buf, "%d %f %s", &(tmp.n), &(tmp.score), tmp.arr);
-	printf("%d %f %s", tmp.n, tmp.score, tmp.arr);
+	struct S s = { 0 };
+	FILE* pf = fseek();
 	return 0;
 }
