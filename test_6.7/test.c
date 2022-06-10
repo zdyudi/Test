b5910911ea954b624019160e47a4e0ef4414879e
zdyudi <1175813308@qq.com>
2022-06-10 14:08:11 +0800

    <vs>

--- test_6.7/test.c
+++ test_6.7/test.c
@@ -119,14 +119,67 @@
 //	return 0;
 //}
 
-struct S
-{
-	int age;
-};
+//int main()
+//{
+//	FILE* pf = fopen("test.txt","r");
+//	if (pf == NULL)
+//	{
+//		return 0;
+//	}
+//	//定位文件指针
+//	fseek(pf, -2, SEEK_END);
+//	//读取文件
+//	int ch = fgetc(pf);
+//	printf("%c\n", ch);
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+//int main()
+//{
+//	FILE* pf = fopen("test.txt", "r");
+//	if (pf == NULL)
+//	{
+//		return 0;
+//	}
+//	//定位文件指针
+//	//fseek(pf, -2, SEEK_END);
+//	int ch = fgetc(pf);
+//	printf("%c\n", ch);
+//	//int pos = ftell(pf);
+//	//printf("%d\n", pos);
+//	rewind(pf);
+//	fgetc(pf);
+//	printf("%c\n", ch);
+//
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
 
 int main()
 {
-	struct S s = { 0 };
-	FILE* pf = fseek();
+	FILE* pf = fopen("test.txt", "r");
+	if (pf == NULL)
+	{
+		perror("hehe");
+		return 0;
+	}
+	int ch = 0;
+	while ((ch = fgetc(pf))!=EOF)
+	{
+		putchar(ch);
+	}
+	if (ferror(pf))
+	{
+		printf("error\n");
+	}
+	else if (feof(pf))
+	{
+		printf("end of file\n");
+	}
+	fclose(pf);
+	pf = NULL;
 	return 0;
 }
