cc2c843b52a92694b1ff5e693f39c5517bb420ef
zdyudi <1175813308@qq.com>
2022-06-24 14:12:22 +0800

    <vs>

--- test_6.18/test.c
+++ test_6.18/test.c
@@ -239,20 +239,56 @@
 //}
 
 //#include<stddef.h>
-#define OFFSETOF(struct_name,member_name) (int)&(((struct_name*)0)->member_name)
+//#define OFFSETOF(struct_name,member_name) (int)&(((struct_name*)0)->member_name)
 
-struct S
-{
-	char c1;
-	int a;
-	char c2;
-};
+//struct S
+//{
+//	char c1;
+//	int a;
+//	char c2;
+//};
+//
+//int main()
+//{
+	//struct S s;
+//	printf("%d\n", OFFSETOF(struct S, c1));//0
+//	printf("%d\n", OFFSETOF(struct S, a));//4
+//	printf("%d\n", OFFSETOF(struct S, c2));//8
+//	return 0;
+//}
+
+//union Un
+//{
+//	short s[7];
+//	int n;
+//};
+//
+//int main()
+//{
+//	printf("%zd\n", sizeof(union Un));
+//	return 0;
+//}
+
+//int main()
+//{
+//	union
+//	{
+//		short k;
+//		char i[2];
+//	}*s,a;
+//	s = &a;
+//	s->i[0] = 0x39;
+//	s->i[1] = 0x38;
+//	printf("%x\n", a.k);
+//	return 0;
+//}
 
 int main()
 {
-	//struct S s;
-	printf("%d\n", OFFSETOF(struct S, c1));//0
-	printf("%d\n", OFFSETOF(struct S, a));//4
-	printf("%d\n", OFFSETOF(struct S, c2));//8
+	int arr[10] = { 1,2,3,4,5,1,2,3,4,6 };
+	int a = 0;
+	int b = 0;
+	int sum = 0;
+
 	return 0;
 }
